05de790b23f192d523ed0a6acb37ea211cfbeeb1
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-12 17:56:10 +0100

    Starting the amazon API class

--- main.py
+++ main.py
@@ -50,3 +50,30 @@ class GenerateReport:
 			print(e)
 			print("Problem with sorting items")
 			return None
+
+
+class AmazonAPI:
+	def __init__(self, search_term, filters, base_url, currency):
+		self.base_url = base_url
+		self.search_term = search_term
+		options = get_web_driver_options()
+		# set_automation_as_head_less(options)
+		set_ignore_certificate_error(options)
+		set_browser_as_incognito(options)
+		self.driver = get_chrome_web_driver(options)
+		self.currency = currency
+		self.price_filter = f"&rh=p_36%3A{filters['min']}00-{filters['max']}00"
+
+	def run(self):
+		print("Starting Script...")
+		print(f"Looking for {self.search_term} products...")
+		links = self.get_products_links()
+		if not links:
+			print("Stopped script.")
+			return
+		print(f"Got {len(links)} links to products...")
+		print("Getting info about products...")
+		products = self.get_products_info(links)
+		print(f"Got info about {len(products)} products...")
+		self.driver.quit()
+		return products
